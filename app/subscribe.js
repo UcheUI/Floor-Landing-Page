1189250d8054d25201d7f39e239b67c2a26a9fd6
uche_ui <uchephillip098@gmail.com>
2023-03-24 15:51:26 +0100

    Created Next App with Stripe Integration

---
+++ app/subscribe.js
@@ -0,0 +1,13 @@
+export default function Subscribe() {
+  return (
+    <div>
+      <h1>Subscription</h1>
+      <p>Thanks for subscribing</p>
+      <span>
+        Click the link below to download Floor and discover a world filled with
+        opportunities
+      </span>
+      <a href="#">Download Now</a>
+    </div>
+  );
+}
